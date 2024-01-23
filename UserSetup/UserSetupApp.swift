cfd7cab43c21ba7bef662173d2e33af5f86dfad4
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-23 09:07:56 +0530

    Initial Commit

---
+++ UserSetup/UserSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserSetupApp.swift
+//  UserSetup
+//
+//  Created by Anuradha Desilva on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UserSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
